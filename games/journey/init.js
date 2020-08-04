9cae6e5269f77251013cc925eed663aff62eebb3
brisberg <brandon.risberg@gmail.com>
2020-08-04 12:42:46 -0700

    Added basic game shell for Journey

---
+++ games/journey/init.js
@@ -0,0 +1,16 @@
+// Game Name key (for analytics)
+window.GAME_NAME = 'journey';
+
+// Setup Initial Static Data
+setup = {
+  initialState: {
+    hull: 20,
+    crew: 10,
+    fuel: 8,
+  }
+};
+
+// Setup custom functions
+window.customPrint = () => {
+  return 'This is a custom print';
+};
